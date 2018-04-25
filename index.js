047d933e8366d99f562903550b668c8172609e0c
Helen Liutongco <liutongcoh@gmail.com>
2018-04-24 22:00:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(endYear) {
+    const startYear = this.startDate.getFullYear()
+
+    return endYear - startYear
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const vertical = this.endingLocation["vertical"] - this.beginningLocation["vertical"]
+    const horizontal = eastWest.indexOf(this.endingLocation["horizontal"]) - eastWest.indexOf(this.beginningLocation["horizontal"])
+    return vertical + horizontal
+  }
+
+  estimatedTime(peak) {
+    const distance = this.blocksTravelled()
+    if(peak) {
+      return distance/2
+    }
+    else {
+      return distance/3
+    }
+  }
+}
