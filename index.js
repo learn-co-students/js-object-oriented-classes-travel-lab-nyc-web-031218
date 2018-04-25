8ba934ce2fa242ba5fe1a97806fac22afc3e5129
Eric Kim <hyeokjungkim@gmail.com>
2018-04-25 11:52:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    let horizontal = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    return vertical + horizontal
+  }
+
+  estimatedTime(rushhour = false){
+    let blocks = this.blocksTravelled()
+    if(rushhour){
+      return Math.floor(blocks/2.0)
+    } else{
+      return Math.floor(blocks/3.0)
+    }
+  }
+}
